b278b40155339fe9c8020148d6cb583da40c1e12
Alan Kleber <alankle@gmail.com>
2020-12-04 11:52:42 +0100

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import React, { useState } from "react";
+import ProgressBar from './components/ProgressBar';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const lessonsList = [
+    { id: 1, lesson: 'State', completed: false },
+    { id: 2, lesson: 'UseRef', completed: false },
+    { id: 3, lesson: 'Hooks personalizados', completed: false },
+    { id: 4, lesson: 'Eventos', completed: false },
+    { id: 5, lesson: 'Pulir aplicación', completed: false },
+   ];
+   
+   const [lessons, setCompleted] = useState(lessonsList);
+
+   const onCompleted = (id, completed) => {
+     const updatedList =lessons.map(lesson =>{
+       if (lesson.id === id) {
+         lesson.completed = !completed;
+       }
+       return lesson
+     });
+     console.log(updatedList);
+     setCompleted(updatedList);
+   }
+
+   const lessonsCompleted = lessons.filter(l => l.completed).length;
+   const percentage = (100 * lessonsCompleted) / lessons.length;
+
+  return (    
     <div className="App">
-      <header className="App-header">
+      <div className="App-wrapper">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3 className="Title">Mi applicacion React</h3>
+          <div className="LessonCheck">
+          {
+            lessons.map(({lesson, id, completed}) => (
+              <label key={id}> 
+                <input type="checkbox"
+                        onChange={() => onCompleted(id, completed)}/>
+
+                
+                <span/>
+                  {lesson}
+                
+              </label>
+            ))
+          }
+          </div>
+          <ProgressBar percentage={percentage}></ProgressBar>
+      </div>
     </div>
   );
 }
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProgressBar =  ({percentage} ) => (
+    <div>
+        {percentage}%
+    </div>
+);
+
+export default ProgressBar;
